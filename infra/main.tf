26f9854be84d2e8f64fe4dbe0498c8364324f3e7
Aurélien <51004295+smeelock@users.noreply.github.com>
2023-02-01 16:06:22 +0100

    feat(infra): add GCP VM as terraform

---
+++ infra/main.tf
@@ -0,0 +1,45 @@
+locals {
+  project_name = "speechemotionrecognition"
+  project_id = "adroit-crow-376514"
+}
+
+terraform {
+  required_providers {
+    google = {
+      version = "~> 2.13.0"
+    }
+  }
+  required_version = "~> 0.12.29"
+}
+
+provider "google" {
+  project = local.project_id
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "${local.project_name}-instance"
+  machine_type = "e2-micro"
+
+  tags = ["dev"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+
+  }
+
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
+}
