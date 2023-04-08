81993fa663ed52b30d0aca8992fbe2609dbdda1f
Aurélien <51004295+smeelock@users.noreply.github.com>
2023-04-08 14:23:12 +0200

    feat: initialize default constants from training script

--- src/constants.py
+++ src/constants.py
@@ -1,1 +1,36 @@
+import os
+
+from sklearn.metrics import accuracy_score, balanced_accuracy_score, f1_score
+
+# dataset
 DEFAULT_TARGET_SAMPLING_RATE = 16000
+DEFAULT_IEMOCAP_LABEL_LIST = ["neu", "hap", "ang", "sad", "exc", "fru"]
+DEFAULT_IEMOCAP_NUM_LABELS = len(DEFAULT_IEMOCAP_LABEL_LIST)
+DEFAULT_IEMOCAP_LABEL2ID = {label: i for i, label in enumerate(DEFAULT_IEMOCAP_LABEL_LIST)}
+DEFAULT_IEMOCAP_ID2LABEL = {i: label for i, label in enumerate(DEFAULT_IEMOCAP_LABEL_LIST)}
+
+# wandb
+DEFAULT_WANDB_WATCH = "all"
+DEFAULT_WANDB_LOG_MODEL = "checkpoint"
+
+# paths
+PACKAGE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DEFAULT_DATA_DIR = os.path.join(PACKAGE_DIR, "data/raw")
+DEFAULT_OUTPUT_DIR = os.path.join(PACKAGE_DIR, "runs/")
+DEFAULT_LOGS_DIR = os.path.join(PACKAGE_DIR, "logs/")
+
+# training
+DEFAULT_DEBUG_SIZE = 1  # 0.1 = 10% of the dataset
+DEFAULT_TEST_SPLIT_SIZE = 0.2
+
+# evaluation
+DEFAULT_METRICS = {
+    "unweighted_accuracy": accuracy_score,
+    "weighted_accuracy": balanced_accuracy_score,
+    "micro_f1": lambda y_true, y_pred: f1_score(y_true, y_pred, average="micro"),
+    "macro_f1": lambda y_true, y_pred: f1_score(y_true, y_pred, average="macro")
+}
+
+# models
+DEFAULT_WHISPER_MODEL_NAME = "openai/whisper-base"
+DEFAULT_WAV2VEC2_MODEL_NAME = "facebook/wav2vec2-base-960h"
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,10 @@
+import numpy as np
+from transformers import EvalPrediction
+
+from constants import DEFAULT_METRICS
+
+
 def compute_metrics(p: EvalPrediction):
     preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
     preds = np.argmax(preds, axis=1)
-    return {k: metric(p.label_ids, preds) for k, metric in metrics.items()}
+    return {k: metric(p.label_ids, preds) for k, metric in DEFAULT_METRICS.items()}
