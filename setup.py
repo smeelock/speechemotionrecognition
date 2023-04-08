f411fffee493e544b812ea213230ba8d73725e59
Aurélien <51004295+smeelock@users.noreply.github.com>
2023-04-08 18:23:03 +0200

    feat: create a python package

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Speech Emotion Recognition',
+    url='https://github.com/smeelock/speechemotionrecognition',
+    packages=find_packages(include=['src', 'src.*']),
+)
