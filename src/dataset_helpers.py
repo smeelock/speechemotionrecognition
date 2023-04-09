757ad4a0fe527eb2d53a89f64e92843b9ff673d1
Aurélien <51004295+smeelock@users.noreply.github.com>
2023-04-09 20:25:41 +0200

    feat: create helper function to get IEMOCAP as huggingface Dataset

--- src/dataset_helpers.py
+++ src/dataset_helpers.py
@@ -1,10 +1,13 @@
 import os
-import torch
-from datasets import Dataset, Audio, Value
+import re
 from pathlib import Path
 
-from constants import DEFAULT_TARGET_SAMPLING_RATE, DEFAULT_IEMOCAP_LABEL_LIST
+from datasets import Dataset, Value, Features
 
+from constants import DEFAULT_IEMOCAP_LABEL_LIST
+
+
+# inspired by https://github.com/pytorch/audio/blob/main/torchaudio/datasets/iemocap.py
 def _get_wavs_paths(data_dir):
     wav_dir = data_dir / "sentences" / "wav"
     wav_paths = sorted(str(p) for p in wav_dir.glob("*/*.wav"))
@@ -15,34 +18,29 @@ def _get_wavs_paths(data_dir):
         relative_paths.append(wav_path)
     return relative_paths
 
-def _create_metadata_csv(root_dir):
+
+def _get_metadata(root):
     root = Path(root)
-    self._path = root / "IEMOCAP"
-    assert os.path.isdir(self._path), "Dataset not found."
+    path = root / "IEMOCAP"
+    assert os.path.isdir(path), "Dataset not found."
 
-    sessions = (1,2,3,4,5)
-
-    all_data = []
-    data = []
-    mapping = []
+    sessions = (1, 2, 3, 4, 5)
+    tmp = []  # will contain all wav file stems
+    metadata = {}  # will contain all metadata
 
     for session in sessions:
         session_name = f"Session{session}"
-        session_dir = self._path / session_name
+        session_dir = path / session_name
 
         # get wav paths
         wav_paths = _get_wavs_paths(session_dir)
         for wav_path in wav_paths:
             wav_stem = str(Path(wav_path).stem)
-            all_data.append(wav_stem)
+            tmp.append(wav_stem)
 
-        # add labels
+        # add label and speaker information
         label_dir = session_dir / "dialog" / "EmoEvaluation"
         query = "*.txt"
-        if utterance_type == "scripted":
-            query = "*script*.txt"
-        elif utterance_type == "improvised":
-            query = "*impro*.txt"
         label_paths = label_dir.glob(query)
 
         for label_path in label_paths:
@@ -53,49 +51,31 @@ def _create_metadata_csv(root_dir):
                     line = re.split("[\t\n]", line)
                     wav_stem = line[1]
                     label = line[2]
-                    if wav_stem not in all_data:
+                    if wav_stem not in tmp:  # only use data that has a wav file
                         continue
-                    if label not in DEFAULT_IEMOCAP_LABEL_LIST:
+                    if label not in DEFAULT_IEMOCAP_LABEL_LIST:  # only use labels that are in the default list
                         continue
-                    mapping[wav_stem] = {}
-                    mapping[wav_stem]["label"] = label
+                    metadata[wav_stem] = {}
+                    metadata[wav_stem]["label"] = label
+                    metadata[wav_stem]["speaker"] = wav_stem.split("_")[0]
 
         for wav_path in wav_paths:
             wav_stem = str(Path(wav_path).stem)
-            if wav_stem in mapping:
-                data.append(wav_stem)
-                mapping[wav_stem]["path"] = wav_path
+            if wav_stem in metadata:
+                metadata[wav_stem]["file_name"] = wav_path
 
-features = Dataset.Features({
-    "wav": "",
-    "path": Value(dtype='string', id=None),
-    "sample_rate": Value(dtype='int', id=None),
-    "label": Value(dtype='string', id=None),
-    "speaker": Value(dtype='string', id=None)
-})
-iemocap = Dataset.from_dict("audiofolder", data_files=all_wav_paths)
+    return metadata
 
-class ProcessedIEMOCAP(torch.utils.data.Dataset):
-    def __init__(self, data: IEMOCAP, processor, config):
-        self.data = data
-        self.processor = processor
-        self.config = config
 
-        if hasattr(config, "target_sampling_rate"):
-            self.target_sampling_rate = config.target_sampling_rate
-        elif (
-            hasattr(self.processor, "feature_extractor")
-            and hasattr(self.processor.feature_extractor, "sampling_rate")
-        ):
-            self.target_sampling_rate = self.processor.feature_extractor.sampling_rate
-        else:
-            self.target_sampling_rate = DEFAULT_TARGET_SAMPLING_RATE
+def get_iemocap(root):
+    metadata = _get_metadata(root)
+    data_files = [m["file_name"] for m in metadata.values()]
 
-    def __getitem__(self, index):
-        wav, _, _, label, _ = self.data[index]
-        inputs = self.processor(wav.squeeze(), sampling_rate=self.target_sampling_rate)
-        inputs["labels"] = self.config.label2id[label]
-        return inputs
-
-    def __len__(self):
-        return len(self.data)
+    features = Features({
+        "wav": "",
+        "path": Value(dtype='string', id=None),
+        "sample_rate": Value(dtype='int', id=None),
+        "label": Value(dtype='string', id=None),
+        "speaker": Value(dtype='string', id=None)
+    })
+    return Dataset.from_dict("audiofolder", data_files=data_files)
