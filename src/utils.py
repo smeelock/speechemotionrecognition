0e6d4cc820b34e8e6d2676f2a6cf2e0df8f9204e
Aurélien <51004295+smeelock@users.noreply.github.com>
2023-04-16 17:00:48 +0200

    chore: clean debug lines

--- src/utils.py
+++ src/utils.py
@@ -4,8 +4,8 @@ from transformers import EvalPrediction
 
 def get_compute_metrics(metrics):
     def _compute_metrics(p: EvalPrediction):
-        print('*'*30, "metrics!")
         preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
         preds = np.argmax(preds, axis=1)
         return {k: metric(p.label_ids, preds) for k, metric in metrics.items()}
+
     return _compute_metrics
