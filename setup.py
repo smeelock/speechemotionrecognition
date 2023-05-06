75f279bc5a5ff63462691087a442adb673533fea
Aurélien <51004295+smeelock@users.noreply.github.com>
2023-05-06 15:24:03 +0200

    ci: build python package in github actions job

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="src",
-    packages=find_packages(),
-    version="0.1.0",
+    name="speechemotionrecognition",
+    description="An experimental project to understand speech signal representation for Speech Emotion Recognition (SER).",
+    python_requires=">=3.8",
+    packages=["speechemotionrecognition"],
+    version="0.0.0",
     author="smeelock",
     license="MIT",
 )
